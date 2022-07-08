a0dc0eb02b75e5b9c1a13266a83b7eea07338f79
Toraudonn <toraudonn@gmail.com>
2022-07-08 13:38:52 +0000

    pad crop

--- configs/image/_base_/datasets/market1501.py
+++ configs/image/_base_/datasets/market1501.py
@@ -3,17 +3,22 @@ img_norm_cfg = dict(
 )
 train_pipeline = [
     dict(type="LoadImageFromFile", to_float32=True),
-    # dict(
-    #     type="Resize",
-    #     size=(256, 128),  # (h, w)
-    #     interpolation="bilinear",
-    # ),
     dict(
-        type="ProbRandomResizedCrop",
-        size=(256, 128),
-        scale=(0.888, 1.0),
-        crop_prob=0.5,
+        type="Resize",
+        size=(256, 128),  # (h, w)
+        interpolation="bilinear",
     ),
+    dict(
+        type="RandomCrop",
+        size=(256, 128),
+        padding=[10, 10, 10, 10]
+    ),
+    # dict(
+    #     type="ProbRandomResizedCrop",
+    #     size=(256, 128),
+    #     scale=(0.95, 1.0),
+    #     crop_prob=0.5,
+    # ),
     dict(
         type="RandomFlip",
         flip_prob=0.5,
