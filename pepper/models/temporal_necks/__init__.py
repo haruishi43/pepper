01336d92db1037a9bf5cc78e79a56c9d43242ce9
Toraudonn <toraudonn@gmail.com>
2022-07-09 15:23:28 +0000

    fix

--- pepper/models/temporal_necks/__init__.py
+++ pepper/models/temporal_necks/__init__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from .temporal_pooling import TemporalPooling
+from .temporal_conv_attention import TemporalConvAttention
 from .temporal_attention import TemporalAttention
 from .rnn import RNN
 
 __all__ = [
     "TemporalAttention",
+    "TemporalConvAttention",
     "TemporalPooling",
     "RNN",
 ]
