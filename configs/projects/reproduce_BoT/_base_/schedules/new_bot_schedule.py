9558826cf7af5df3a0fb10a44f80e498c2ba04ce
Toraudonn <toraudonn@gmail.com>
2022-07-13 11:54:27 +0000

    add

--- configs/projects/reproduce_BoT/_base_/schedules/new_bot_schedule.py
+++ configs/projects/reproduce_BoT/_base_/schedules/new_bot_schedule.py
@@ -1,7 +1,10 @@
 # optimizer
-# optimizer = dict(type="Adam", lr=0.00035, weight_decay=5e-04, betas=(0.9, 0.99))
+# paramwise_cfg = dict(
+#     custom_keys={}
+# )
+optimizer = dict(type="Adam", lr=0.00035, weight_decay=5e-04, betas=(0.9, 0.99))
 # optimizer = dict(type="Adam", lr=0.00035, weight_decay=0, betas=(0.9, 0.99))
-optimizer = dict(type="AdamW", lr=0.00035, weight_decay=0.01, betas=(0.9, 0.99))
+# optimizer = dict(type="AdamW", lr=0.00035, weight_decay=0.01, betas=(0.9, 0.99))
 
 optimizer_config = dict(grad_clip=dict(max_norm=5.0, norm_type=2))
 
@@ -24,9 +27,9 @@ lr_config = dict(
     step=[2000, 3500, 6000],
     gamma=0.1,
     # -> warmup config
-    # warmup="linear",
-    # warmup_iters=500,
-    # warmup_ratio=0.1,
+    warmup="linear",
+    warmup_iters=1000,
+    warmup_ratio=0.1,
 )
 
 # runner settings
