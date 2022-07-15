bac6c681c6e613419118247fa6b447e1881d32f4
Toraudonn <toraudonn@gmail.com>
2022-07-15 10:17:37 +0000

    add configs

--- configs/projects/reproduce_fastreid/agw/agw_r50_default_market1501.py
+++ configs/projects/reproduce_fastreid/agw/agw_r50_default_market1501.py
@@ -1,9 +1,10 @@
 _base_ = [
     "../_base_/models/agw_resnet50.py",
     "../_base_/datasets/market1501_REA.py",
-    "../_base_/schedules/bot_schedule.py",
+    "../_base_/schedules/default_schedule.py",
     "../_base_/default_runtime.py",
 ]
+# baseline-s + warmup + REA + LS + stride=1 + BNNeck
 data = dict(
     samples_per_gpu=64,
     workers_per_gpu=8,
