42bd4f8bbce9818db162307148bc4920a967021d
Toraudonn <toraudonn@gmail.com>
2022-07-15 00:05:42 +0000

    update scheduling

--- configs/projects/reproduce_fastreid/agw/agw_r50_market1501.py
+++ configs/projects/reproduce_fastreid/agw/agw_r50_market1501.py
@@ -1,7 +1,7 @@
 _base_ = [
     "../_base_/models/agw_resnet50.py",
     "../_base_/datasets/market1501_REA.py",
-    "../_base_/schedules/bot_schedule.py",
+    "../_base_/schedules/new_bot_schedule.py",
     "../_base_/default_runtime.py",
 ]
 # baseline-s + warmup + REA + LS + stride=1 + BNNeck
