fa4ff64bb4c8c557e5e5fa3d08cbe3926572db39
Toraudonn <toraudonn@gmail.com>
2022-07-16 09:42:38 +0000

    edit

--- configs/projects/reproduce_fastreid/agw/nonlocal_r50_market1501.py
+++ configs/projects/reproduce_fastreid/agw/nonlocal_r50_market1501.py
@@ -1,10 +1,10 @@
 _base_ = [
-    "../_base_/models/basic_resnet50.py",
     "../_base_/datasets/market1501_REA.py",
     "../_base_/schedules/bot_schedule.py",
     "../_base_/default_runtime.py",
 ]
 model = dict(
+    type="ImageReID",
     backbone=dict(
         type="BetterPlugResNet",
         depth=50,
@@ -28,4 +28,29 @@ model = dict(
         ],
     ),
     neck=dict(type="GeneralizedMeanPooling"),
+    head=dict(
+        type="BoTReIDHead",
+        in_channels=2048,
+        num_classes=380,
+        loss=dict(
+            type="LabelSmoothLoss", label_smooth_val=0.1, loss_weight=1.0
+        ),
+        loss_pairwise=dict(type="TripletLoss", margin=0.0, hard_mining=False, loss_weight=1.0),
+        # loss_circle=dict(type="CircleLoss", margin=0.25, gamma=128, loss_weight=1.0),
+        norm_cfg=dict(type="BN1d"),
+        act_cfg=dict(type="ReLU"),
+    ),
+    init_cfg=dict(
+        type="Pretrained",
+        checkpoint="https://download.openmmlab.com/mmclassification/v0/resnet/resnet50_batch256_imagenet_20200708-cfb998bf.pth",  # noqa: E251  # noqa: E501
+    ),
+    inference_stage="pre_logits",
+)
+data = dict(
+    samples_per_gpu=64,
+    workers_per_gpu=8,
+)
+sampler = dict(
+    batch_size=64,
+    num_instances=4,
 )
