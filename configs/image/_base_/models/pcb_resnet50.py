0fb9ce86cb74d48a5d19d0d97e5e51f6cfa2b7cf
Toraudonn <toraudonn@gmail.com>
2022-10-14 09:11:00 +0000

    fix config

--- configs/image/_base_/models/pcb_resnet50.py
+++ configs/image/_base_/models/pcb_resnet50.py
@@ -15,12 +15,7 @@ model = dict(
             prefix="backbone.",
         ),
     ),
-    # neck=dict(type="PartPooling", num_parts=num_parts),
-    neck=dict(
-        type="RefinedPartPooling",
-        num_parts=num_parts,
-        in_channels=in_channels,
-    ),
+    neck=dict(type="PartPooling", num_parts=num_parts),
     head=dict(
         type="PCBHead",
         num_parts=num_parts,
---
+++ configs/image/pcb/pcb_rpp_resnet50_b64_market1501.py
@@ -0,0 +1,24 @@
+_base_ = [
+    "../_base_/models/pcb_resnet50.py",
+    "../_base_/samplers/infinite_balanced.py",
+    "../_base_/datasets/market1501.py",
+    "../_base_/schedules/bot_schedule.py",
+    "../_base_/default_runtime.py",
+]
+num_parts = 4
+in_channels = 2048
+model = dict(
+    neck=dict(
+        type="RefinedPartPooling",
+        num_parts=num_parts,
+        in_channels=in_channels,
+    ),
+)
+data = dict(
+    samples_per_gpu=64,
+    workers_per_gpu=8,
+)
+sampler = dict(
+    batch_size=64,
+    num_instances=4,
+)
